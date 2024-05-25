9086292b5ee62b0ebccb6dad6143e5e049ee7a17
zarkob <zarkob@users.noreply.github.com>
2024-05-26 00:56:41 +0200

    init modif only to test Service Worker

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'tesla-configurator' title`, () => {
+  it(`should have the 'tesla-configurator-test' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('tesla-configurator');
+    expect(app.title).toEqual('tesla-configurator-test');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, tesla-configurator');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, tesla-configurator-test');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,25 @@
-import {Component} from '@angular/core';
-import {AsyncPipe, JsonPipe} from '@angular/common';
+import { Component } from '@angular/core';
+import {AsyncPipe, JsonPipe, NgIf} from '@angular/common';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [AsyncPipe, JsonPipe],
+  imports: [AsyncPipe, JsonPipe, HttpClientModule, NgIf],
   template: `
     <h1>Hello from {{ name }}!</h1>
+    <div *ngIf="models$ | async as models; else loading">
+      <pre>{{ models | json }}</pre>
+    </div>
+    <ng-template #loading>Loading models...</ng-template>
   `,
 })
 export class AppComponent {
   name = 'Angular';
+  models$: Observable<any>;
 
+  constructor(private http: HttpClient) {
+    this.models$ = this.http.get('/models');
+  }
 }
